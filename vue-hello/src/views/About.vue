103adcb878c8852f8ca1f315b244830edde5c701
Harrylj <648267773@qq.com>
2020-06-09 11:28:59 +0800

    提交

--- vue-hello/src/views/About.vue
+++ vue-hello/src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page 2020-06-09 11:28</h1>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+// import router from '@/router'
+export default {
+  name: "login",
+  data() {
+    return {
+      tel: "",
+      sms: "",
+      num_01: "",
+      num_02: "",
+    };
+  },
+  components: {},
+  methods: {
+
+  },
+  created() {
+    this.$http
+        // .get("https://mall.dc.sdtzcd.com/api/home/GetAgreement?app_key=SDMALLKEY&agreementTypes=3&userkey=ZUh4NmlMb2JrdzZkTm9OQ0M0QWZBUWg2V1dxRDNQZmJ3bVN1aFlVTm8rc0t2ZktJVWNhSDI0TGFyd2RXUStlZg%3D%3D&timestamp=1590464364&sign=3AFB88C7BFF1174F71AC52CA8AE175C1")
+        .get("https://mall.dc.sdtzcd.com/api/home/GetAgreement?app_key=SDMALLKEY&agreementTypes=3&userkey=ZUh4NmlMb2JrdzZkTm9OQ0M0QWZBUWg2V1dxRDNQZmJ3bVN1aFlVTm8rc0t2ZktJVWNhSDI0TGFyd2RXUStlZg%3D%3D&timestamp=1590464364&sign=3AFB88C7BFF1174F71AC52CA8AE175C1")
+        .then(res => {
+          if (res) {
+            console.log('999- ',res.data)
+             this.content = res.msg
+             
+            // alert(res.data.InfoMsg);
+          }
+        });
+  },
+};
+</script>
--- vue-hello/src/views/Home.vue
+++ vue-hello/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     home
+    <h2>{{countAlias}}</h2>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,7 +9,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import {mapState} from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -25,5 +26,17 @@ export default {
     this.increment();
     console.log('22',this)
   },
+  computed: mapState({
+    // 箭头函数可使代码更简练
+    count: state => state.count,
+
+    // 传字符串参数 'count' 等同于 `state => state.count`
+    countAlias: 'count',
+
+    // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+    countPlusLocalState (state) {
+      return state.count + this.localCount
+    }
+  })
 }
 </script>
