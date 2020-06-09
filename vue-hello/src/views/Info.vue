5ca7a34231096c65ccbedde4bc89acec7c56a5b5
Harrylj <648267773@qq.com>
2020-06-09 14:46:15 +0800

    5

--- vue-hello/src/views/Info.vue
+++ vue-hello/src/views/Info.vue
@@ -4,6 +4,8 @@
     <h2>1</h2>
     <h2>2</h2>
     <h3>3</h3>
+    <h4>4</h4>
+    <h5>5</h5>
     <van-button size="small" type="primary" @click="btnLeave" >退出登录</van-button>
   </div>
 </template>
